edfc9bddde4363cac2d9e422a677e819d6959efc
GRY64 <38182562+GRY64@users.noreply.github.com>
2018-07-09 13:27:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -398,7 +398,7 @@ bot.on('message', message => {
 
 bot.on('message', message => {
   if (message.content === '!testColor') {
-		message.guild.createRole({name : "testColor", color : "#800000"});
+		message.guild.createRole({name : "testColor", color : "#FFFFFF"});
 	  	var role = message.guild.roles.find("name", "testColor")
 		message.member.addRole(role)
   }
